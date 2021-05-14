7dd4e2af7a6f810802ae496530e9e453ce295f96
divyanshuGit-collab <84186894+divyanshuGit-collab@users.noreply.github.com>
2021-05-14 19:05:06 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,9 @@
+#program to chk anumber prime or not 
+n = int(input("Enter you number:-")) 
+#user iput taken here:
+if n==2:
+ print("Yes, It's Prime!")           
+elif n%2!=0:
+    print("Yes,it's Prime!")
+else :
+ print("Sorry ,not a prime number!")
